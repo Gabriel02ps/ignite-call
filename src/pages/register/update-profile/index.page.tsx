8a26e97cf06c2c3393e11c7073e52351841f844b
Gabriel02ps <gabriel02ps@gmail.com>
2023-08-23 22:57:01 -0300

    feat(update-profile): add data persistence for profile

--- src/pages/register/update-profile/index.page.tsx
+++ src/pages/register/update-profile/index.page.tsx
@@ -1,4 +1,11 @@
-import { Button, Heading, MultiStep, Text, TextArea } from '@ignite-ui/react'
+import {
+  Avatar,
+  Button,
+  Heading,
+  MultiStep,
+  Text,
+  TextArea,
+} from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
@@ -9,6 +16,8 @@ import { useSession } from 'next-auth/react'
 import { buildNextAuthOptions } from '@/pages/api/auth/[...nextauth].api'
 import { GetServerSideProps } from 'next'
 import { unstable_getServerSession } from 'next-auth'
+import { api } from '@/lib/axios'
+import { useRouter } from 'next/router'
 
 const updateProfileSchema = z.object({
   bio: z.string(),
@@ -26,11 +35,14 @@ export default function UpdateProfile() {
   })
 
   const session = useSession()
-
-  console.log(session)
+  const router = useRouter()
 
   async function handleUpdateProfile(data: updateProfileData) {
-    console.log('handleUpdateProfile')
+    await api.put('/users/profile', {
+      bio: data.bio,
+    })
+
+    await router.push(`/schedule/${session.data?.user.username}`)
   }
 
   return (
@@ -48,6 +60,10 @@ export default function UpdateProfile() {
       <ProfileBox as="form" onSubmit={handleSubmit(handleUpdateProfile)}>
         <label>
           <Text>Foto de perfil</Text>
+          <Avatar
+            src={session.data?.user.avatar_url}
+            alt={session.data?.user.name}
+          />
         </label>
 
         <label>
